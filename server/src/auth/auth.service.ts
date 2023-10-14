ddfac42ecbac6c0c6ed5d413509896ce7d2fab6f
Zulham Ari <ari.comunity1@gmail.com>
2023-10-14 23:57:42 +0700

    update authentification

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,18 +2,30 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(dto: AuthDto) {
     const user = await this.userService.findByEmail(dto.email);
     const pw = await compare(dto.password, user.password);
 
+    const payload = { sub: user.id, username: user.name };
+
     if (user && pw) {
       const { password, ...data } = user;
-      return data;
+      return {
+        ...data,
+        access_token: await this.jwtService.signAsync(payload, {
+          secret: 'landborder',
+          expiresIn: '60s',
+        }),
+      };
     }
 
     throw new UnauthorizedException();
