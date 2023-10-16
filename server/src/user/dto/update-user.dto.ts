c4e74cf1ca62ef4526334ebd6f67a7204eec70c6
Zulham Ari <ari.comunity1@gmail.com>
2023-10-16 22:25:48 +0700

    update prisma model and lib uses

--- server/src/user/dto/update-user.dto.ts
+++ server/src/user/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { RegistUserDto } from './regist-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(RegistUserDto) {}
