da1c8b61a218e8ceffc2c604b17e8cf952120c26
Z4nR <ari.comunity1@gmail.com>
2023-11-08 16:39:50 +0700

    delete unused import

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
