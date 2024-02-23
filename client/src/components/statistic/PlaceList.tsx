c52c1bd0a7091b45437b881a383ceddf2e65f4f8
Z4nR <ari.comunity1@gmail.com>
2024-02-23 08:46:32 +0700

    update place list tab

--- client/src/components/statistic/PlaceList.tsx
+++ client/src/components/statistic/PlaceList.tsx
@@ -1,83 +1,196 @@
-import { Table, Tag } from 'antd';
+import {
+  Button,
+  GetRef,
+  Input,
+  Space,
+  Table,
+  TableColumnType,
+  Tag,
+} from 'antd';
 import type { TableProps } from 'antd/es/table';
+import { SearchOutlined } from '@ant-design/icons';
 import { useQuery } from '@tanstack/react-query';
 import { placeList } from '../../utils/networks';
+import { dateFormatter } from '../../utils/helper';
 import { Link } from 'react-router-dom';
+import { useRef, useState } from 'react';
+import { FilterDropdownProps } from 'antd/es/table/interface';
+import Highlighter from 'react-highlight-words';
 
+type InputRef = GetRef<typeof Input>;
 interface DataType {
   placeId: string;
   placeName: string;
   placeAddress: string;
   placeType: string;
   createdBy: string;
-  createdAt: string;
+  createdAt: Date;
 }
 
-const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'Name',
-    dataIndex: 'placeName',
-    key: 'name',
-    fixed: 'left',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'placeAddress',
-    key: 'address',
-  },
-  {
-    title: 'Tipe',
-    dataIndex: 'placeType',
-    key: 'type',
-    width: '10%',
-    render: (_, tag) => {
-      let color: string = '';
-      if (tag.placeType === 'Bangunan') {
-        color = 'volcano';
-      }
-      return <Tag color={color}>{tag.placeType.toUpperCase()}</Tag>;
-    },
-  },
-  {
-    title: 'Pembuat',
-    dataIndex: 'createdBy',
-    key: 'creator',
-    align: 'center',
-    width: '10%',
-  },
-  {
-    title: 'Ditambahkan Pada',
-    dataIndex: 'createdAt',
-    key: 'date',
-    align: 'center',
-    width: '15%',
-    render: (_, time) => {
-      const date = new Date(time.createdAt).toLocaleDateString('id-ID', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-      });
-      return <p>{date}</p>;
-    },
-  },
-  {
-    title: 'Tindakan',
-    key: 'action',
-    fixed: 'right',
-    align: 'center',
-    width: '10%',
-    render: (_, detail) => (
-      <Link to={`/${detail.placeId}/detil`}>Lihat Detil</Link>
-    ),
-  },
-];
+type DataIndex = keyof DataType;
 
 const PlaceList: React.FC = () => {
   const { data } = useQuery({
     queryKey: ['place-all'],
     queryFn: async () => await placeList(),
   });
+
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: FilterDropdownProps['confirm'],
+    dataIndex: DataIndex
+  ) => {
+    confirm({ closeDropdown: true });
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (
+    dataIndex: DataIndex
+  ): TableColumnType<DataType> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Cari
+          </Button>
+          <Button
+            onClick={() => {
+              clearFilters && handleReset(clearFilters);
+              confirm({ closeDropdown: true });
+            }}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Hapus
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            Tutup
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns: TableProps<DataType>['columns'] = [
+    {
+      title: 'Nama Tempat',
+      dataIndex: 'placeName',
+      key: 'place-name',
+      fixed: 'left',
+      ...getColumnSearchProps('placeName'),
+    },
+    {
+      title: 'Alamat',
+      dataIndex: 'placeAddress',
+      key: 'place-address',
+    },
+    {
+      title: 'Tipe',
+      dataIndex: 'placeType',
+      key: 'place-type',
+      width: '10%',
+      render: (_, tag) => {
+        let color: string = '';
+        if (tag.placeType === 'Bangunan') {
+          color = 'volcano';
+        }
+        return <Tag color={color}>{tag.placeType.toUpperCase()}</Tag>;
+      },
+    },
+    {
+      title: 'Oleh',
+      dataIndex: 'createdBy',
+      key: 'place-creator',
+      align: 'center',
+      width: '10%',
+    },
+    {
+      title: 'Ditambahkan Pada',
+      dataIndex: 'createdAt',
+      key: 'place-create',
+      align: 'center',
+      width: '15%',
+      sorter: (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+      sortDirections: ['descend', 'ascend'],
+      defaultSortOrder: 'ascend',
+      render: (_, time) => {
+        const date = dateFormatter(time.createdAt);
+        return <p>{date}</p>;
+      },
+    },
+    {
+      title: 'Tindakan',
+      key: 'place-action',
+      fixed: 'right',
+      align: 'center',
+      width: '10%',
+      render: (_, detail) => (
+        <Link to={`/${detail.placeId}/detil`}>Lihat Detil</Link>
+      ),
+    },
+  ];
 
   return (
     <Table
@@ -86,7 +199,6 @@ const PlaceList: React.FC = () => {
       style={{ backgroundColor: 'transparent' }}
       columns={columns}
       dataSource={data}
-      pagination={{ position: ['bottomCenter'] }}
     />
   );
 };
