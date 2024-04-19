7d77669d7f6c10db41d096aa91c5cb7048ba7438
Z4nR <ari.comunity1@gmail.com>
2024-04-20 00:17:50 +0700

    update auth service

--- server/src/application/auth/auth.service.ts
+++ server/src/application/auth/auth.service.ts
@@ -11,6 +11,7 @@ import { ConfigService } from '@nestjs/config';
 import { compare, hash } from 'bcrypt';
 import { AuthRegistDto, ChangePasswordDto } from './dto/auth.dto';
 import { HelperService } from '../helper-service/helper.service';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class AuthService {
@@ -25,7 +26,7 @@ export class AuthService {
     try {
       return await this.jwtService.signAsync(payload, {
         secret: this.configService.get('SECRET'),
-        expiresIn: '6h',
+        expiresIn: '12h',
       });
     } catch (error) {
       console.log(error);
@@ -34,6 +35,13 @@ export class AuthService {
   }
 
   async changePassword(dto: ChangePasswordDto) {
+    const user = await this.helperService.findByUsername(dto.username);
+    if (!user) throw new NotFoundException('Nama Pengguna tidak diketahui');
+
+    const code = await compare(dto.code, user.special_code);
+    if (!code)
+      throw new BadRequestException('Kode yang anda masukan tidak sesuai');
+
     try {
       await this.prisma.user.update({
         where: {
@@ -52,9 +60,6 @@ export class AuthService {
   }
 
   async register(dto: AuthRegistDto) {
-    if (dto.password !== dto.verify)
-      throw new BadRequestException('Verifikasi Password Tidak Sesuai');
-
     const username = await this.helperService.findByUsername(dto.username);
     if (username) throw new ConflictException('Nama Pengguna sudah digunakan');
 
@@ -63,6 +68,7 @@ export class AuthService {
         data: {
           user_name: dto.username,
           full_name: dto.fullname,
+          special_code: await hash(dto.code, 10),
           password: await hash(dto.password, 10),
         },
       });
@@ -89,6 +95,7 @@ export class AuthService {
         sub: user.user_id,
         user: user.user_name,
         role: user.admin,
+        id: randomUUID(),
       };
 
       const createToken = await this.createToken(payload);
