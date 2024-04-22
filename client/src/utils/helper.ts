03694c9921584823d2f3f08d831dc52bf49ea1eb
Z4nR <ari.comunity1@gmail.com>
2024-04-22 18:40:00 +0700

    update helper file

--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -13,16 +13,24 @@ const getGreeting = () => {
 
   let greeting;
 
-  if (currentHour < 5) {
-    greeting = 'Kamu Tidak Tidur';
-  } else if (currentHour >= 5 && currentHour < 12) {
-    greeting = 'Selamat Pagi';
-  } else if (currentHour >= 12 && currentHour < 17) {
-    greeting = 'Selamat Siang';
-  } else if (currentHour >= 17 && currentHour < 20) {
-    greeting = 'Selamat Sore';
-  } else {
-    greeting = 'Selamat Malam';
+  switch (true) {
+    case currentHour < 5:
+      greeting = 'Kamu Tidak Tidur';
+      break;
+    case currentHour >= 5 && currentHour < 10:
+      greeting = 'Selamat Pagi';
+      break;
+    case currentHour >= 10 && currentHour < 15:
+      greeting = 'Selamat Siang';
+      break;
+    case currentHour >= 15 && currentHour < 18:
+      greeting = 'Selamat Sore';
+      break;
+    case currentHour >= 18 && currentHour < 22:
+      greeting = 'Selamat Malam';
+      break;
+    default:
+      greeting = 'Selamat Malam';
   }
 
   return greeting;
