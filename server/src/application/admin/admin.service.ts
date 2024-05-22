5c70f890e1a62e97694255619bca74033b6390ac
Z4nR <ari.comunity1@gmail.com>
2024-05-23 00:40:14 +0700

    update admin servce

--- server/src/application/admin/admin.service.ts
+++ server/src/application/admin/admin.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
+import { PrismaService } from 'src/db/prisma.service';
 
 @Injectable()
 export class AdminService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createAdminDto: CreateAdminDto) {
     return 'This action adds a new admin';
   }
 
-  findAll() {
-    return `This action returns all admin`;
+  async findAll() {
+    try {
+      return await this.prisma.user.findMany({
+        select: {
+          user_id: true,
+          user_name: true,
+          description: true,
+          admin: true,
+          created_at: true,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
   findOne(id: number) {
@@ -20,7 +36,87 @@ export class AdminService {
     return `This action updates a #${id} admin`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} admin`;
+  async removePlace(id: string) {
+    try {
+      await this.prisma.$transaction(async (tx) => {
+        const mapId = await tx.placeData.findUnique({
+          select: {
+            map_id: true,
+          },
+          where: {
+            place_id: id,
+          },
+        });
+
+        await tx.placeMap.delete({
+          where: {
+            map_id: mapId.map_id,
+          },
+        });
+
+        await tx.placeData.delete({
+          where: {
+            place_id: id,
+          },
+        });
+      });
+      return { message: 'Data tempat berhasil dihapus oleh admin' };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async removeUser(id: string) {
+    try {
+      await this.prisma.$transaction(async (tx) => {
+        const findPlaceId = await tx.user
+          .findMany({
+            select: {
+              place: {
+                select: {
+                  map_id: true,
+                },
+              },
+            },
+            where: {
+              user_id: id,
+            },
+          })
+          .then((users) =>
+            users.map((user) => user.place.map((place) => place.map_id)),
+          );
+        console.log(findPlaceId);
+
+        const flatPlaceId = findPlaceId.flat();
+
+        await Promise.all(
+          flatPlaceId.map(async (placeid) => {
+            await tx.placeMap.delete({
+              where: {
+                map_id: placeid,
+              },
+            });
+          }),
+        );
+
+        await tx.placeData.deleteMany({
+          where: {
+            user_id: id,
+          },
+        });
+
+        await tx.user.delete({
+          where: {
+            user_id: id,
+          },
+        });
+      });
+
+      return { message: 'Seluruh data pengguna berhasil dihapus oleh admin' };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 }
