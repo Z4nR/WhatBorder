3e4d6462d70676397d9c1942c6c4e545685c1fd7
Z4nR <ari.comunity1@gmail.com>
2024-05-24 00:23:40 +0700

    update admin module

--- server/src/application/admin/admin.module.ts
+++ server/src/application/admin/admin.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { PrismaService } from 'src/db/prisma.service';
+import { HelperService } from '../helper-service/helper.service';
+import { PlaceService } from '../place/place.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, PrismaService, HelperService, PlaceService],
 })
 export class AdminModule {}
