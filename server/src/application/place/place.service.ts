3b47e3496f2d9e4b4cb678fe9615807b90e86d65
Z4nR <ari.comunity1@gmail.com>
2024-05-26 00:41:53 +0700

    update place service

--- server/src/application/place/place.service.ts
+++ server/src/application/place/place.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePlaceDto } from './dto/create-place.dto';
 import { UpdatePlaceDto } from './dto/update-place.dto';
 import { PrismaService } from 'src/db/prisma.service';
+import { Place } from './entities/place.entity';
 
 @Injectable()
 export class PlaceService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(id: string, name: string, createPlaceDto: CreatePlaceDto) {
     try {
@@ -20,11 +21,15 @@ export class PlaceService {
           },
         });
 
+        console.log(placeMap);
+
         await tx.placeData.create({
           data: {
             place_name: createPlaceDto.placeName,
             place_address: createPlaceDto.placeAddress,
             place_type: createPlaceDto.placeType,
+            place_owner: createPlaceDto.placeOwner ?? null,
+            place_description: createPlaceDto.placeDescription ?? null,
             map_id: placeMap.map_id,
             created_by: name,
             user_id: id,
@@ -107,8 +112,64 @@ export class PlaceService {
     }
   }
 
-  update(id: number, updatePlaceDto: UpdatePlaceDto) {
-    return `This action updates a #${id} place`;
+  async update(
+    id: string,
+    userId: string,
+    userName: string,
+    updatePlaceDto: UpdatePlaceDto,
+  ) {
+    const updatePlaceData: Place = {
+      place_name: updatePlaceDto.placeName,
+      place_address: updatePlaceDto.placeAddress,
+      place_description: updatePlaceDto.placeDescription,
+      place_owner: updatePlaceDto.placeOwner,
+      place_type: updatePlaceDto.placeType,
+      updated_by: userName,
+    };
+
+    try {
+      await this.prisma.$transaction(async (tx) => {
+        const mapId = await this.prisma.placeData.findFirst({
+          select: {
+            map_id: true,
+          },
+          where: {
+            place_id: id,
+            user_id: userId,
+          },
+        });
+
+        if (!mapId) {
+          throw new Error(
+            'Place not found or you do not have permission to update it',
+          );
+        }
+
+        await tx.placeData.update({
+          data: updatePlaceData,
+          where: {
+            place_id: id,
+            user_id: userId,
+          },
+        });
+
+        if (updatePlaceDto.placeGeojson !== undefined) {
+          await tx.placeMap.update({
+            data: {
+              place_geojson: updatePlaceDto.placeGeojson,
+              updated_by: userName,
+            },
+            where: {
+              map_id: mapId.map_id,
+            },
+          });
+        }
+      });
+      return { message: 'Data Berhasil Diperbarui' };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
   async remove(id: string, userId: string) {
