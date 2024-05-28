9367165e6c7dcad012f4a21a98e526fef5f3e15e
Z4nR <ari.comunity1@gmail.com>
2024-05-28 08:08:30 +0700

    update user controller

--- server/src/application/user/user.controller.ts
+++ server/src/application/user/user.controller.ts
@@ -6,31 +6,36 @@ import {
   Patch,
   Param,
   Delete,
-  Request,
   HttpCode,
   HttpStatus,
   InternalServerErrorException,
   NotFoundException,
+  Req,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { HelperService } from '../helper-service/helper.service';
+import { Request } from 'express';
+import { Roles } from '../auth/authorize/decorator/role.decorator';
+import { Role } from '../auth/authorize/enum/role.enum';
 
 @Controller('user')
 export class UserController {
   constructor(
     private readonly userService: UserService,
-    private helperService: HelperService,
+    private readonly helperService: HelperService,
   ) {}
 
   @HttpCode(HttpStatus.OK)
   @Get('me')
-  user(@Request() req: any) {
+  user(@Req() req: Request) {
+    const user = req['user'];
+
     try {
       return {
-        username: req.user.user,
-        role: req.user.role,
-        exp: req.user.exp,
+        username: user.user,
+        role: user.role,
+        exp: user.exp,
       };
     } catch (error) {
       console.log(error);
@@ -52,35 +57,41 @@ export class UserController {
   }
 
   @Get('profile')
-  async findMe(@Request() req: any) {
-    const id = req.user.sub;
+  async findMe(@Req() req: Request) {
+    const user = req['user'];
+    const id = user.sub;
 
-    const user = await this.helperService.findByIdUser(id);
-    if (!user) throw new NotFoundException('Pengguna Tidak Ditemukan');
+    const findUser = await this.helperService.findByIdUser(id);
+    if (!findUser) throw new NotFoundException('Pengguna Tidak Ditemukan');
 
     return this.userService.myProfile(id);
   }
 
+  @Roles(Role.USER)
   @Patch('update')
-  async update(@Request() req: any, @Body() updateUserDto: UpdateUserDto) {
-    const id = req.user.sub;
+  async update(@Req() req: Request, @Body() updateUserDto: UpdateUserDto) {
+    const user = req['user'];
+    const id = user.sub;
 
-    const user = await this.helperService.findByIdUser(id);
-    if (!user) throw new NotFoundException('Pengguna Tidak Ditemukan');
+    const findUser = await this.helperService.findByIdUser(id);
+    if (!findUser) throw new NotFoundException('Pengguna Tidak Ditemukan');
 
     return await this.userService.update(id, updateUserDto);
   }
 
+  @Roles(Role.USER)
   @Delete('delete')
-  async remove(@Request() req: any, @Body() data: any) {
-    const id = req.user.sub;
+  async remove(@Req() req: Request, @Body() data: any) {
+    const user = req['user'];
+    const id = user.sub;
 
-    const user = await this.helperService.findByIdUser(id);
-    if (!user) throw new NotFoundException('Pengguna Tidak Ditemukan');
+    const findUser = await this.helperService.findByIdUser(id);
+    if (!findUser) throw new NotFoundException('Pengguna Tidak Ditemukan');
 
-    console.log(data, typeof data);
+    console.log(data);
+
     const { password } = data;
 
-    return await this.userService.remove(id, user.password, password);
+    return await this.userService.remove(id, findUser.password, password);
   }
 }
