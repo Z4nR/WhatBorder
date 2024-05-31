37ca619cdd1bf3b9c77ca3a1a67acd4cf063c522
Z4nR <ari.comunity1@gmail.com>
2024-06-01 06:18:01 +0700

    update place entity

--- server/src/application/place/entities/place.entity.ts
+++ server/src/application/place/entities/place.entity.ts
@@ -1,1 +1,8 @@
-export class Place {}
+export class Place {
+  place_name?: string;
+  place_address?: string;
+  place_type?: string;
+  place_owner?: string;
+  place_description?: string;
+  updated_by: string;
+}
