edf17d3674a2c875fc45c04bb659052168cbd3f1
Z4nR <ari.comunity1@gmail.com>
2024-06-19 08:28:50 +0700

    update env file in client

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_BACKEND: string;
+  readonly VITE_SECRET: string;
+  readonly VITE_USER: string;
 }
 
 interface importMeta {
