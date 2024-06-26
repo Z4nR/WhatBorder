b9ee9abb315f344c7ee8317ad5e9b95eae03ba22
Z4nR <ari.comunity1@gmail.com>
2024-06-26 09:33:54 +0700

    update role guard

--- server/src/application/auth/authorize/role.guard.ts
+++ server/src/application/auth/authorize/role.guard.ts
@@ -2,10 +2,14 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from './decorator/role.decorator';
 import { Role } from './enum/role.enum';
+import { HelperService } from 'src/application/helper-service/helper.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    private readonly helperService: HelperService,
+  ) {}
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
@@ -16,7 +20,7 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const request = context.switchToHttp().getRequest();
-    const role = request['user'].role;
+    const role = this.helperService.findByUsername(request['user'].user);
 
     const admin: string[] = [];
     role ? admin.push('ADMIN') : admin.push('USER');
