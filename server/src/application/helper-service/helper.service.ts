6c65cb0d3980447a7713162b0506670d05dfbfc9
Z4nR <ari.comunity1@gmail.com>
2024-06-30 09:55:27 +0700

    helper service update

--- server/src/application/helper-service/helper.service.ts
+++ server/src/application/helper-service/helper.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/db/prisma.service';
+import * as CryptoJS from 'crypto-js';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class HelperService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  decryptPassword(password: string) {
+    const specialValue = this.configService.get('USER');
+    const fullString = password.indexOf(specialValue);
+
+    if (fullString === -1) {
+      throw new BadRequestException(`Special value not found in '${password}'`);
+    }
+
+    const word = password.slice(fullString + specialValue.length);
+
+    const bytes = CryptoJS.AES.decrypt(word, this.configService.get('SECRET'));
+    console.log(bytes);
+    const parsePassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    return parsePassword;
+  }
 
   async findByUsername(username: string) {
-    console.log(username);
-
     try {
       const data = await this.prisma.user.findFirst({
         where: {
