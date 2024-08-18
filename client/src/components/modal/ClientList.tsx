aa0aec7d6e6b0b348bc375a6b3c7fbda886f4f38
Z4nR <ari.comunity1@gmail.com>
2024-08-18 22:45:41 +0700

    update client list

--- client/src/components/modal/ClientList.tsx
+++ client/src/components/modal/ClientList.tsx
@@ -1,12 +1,29 @@
+import { socketConnection } from '@/utils/helper';
 import { Modal } from 'antd';
+import { useEffect, useState } from 'react';
 
 interface ModalSet {
+  setState: (state: boolean) => void;
   state: boolean;
 }
 
-const ClientList: React.FC<ModalSet> = ({ state }) => {
+const ClientList: React.FC<ModalSet> = ({ state, setState }) => {
+  const socket = socketConnection();
+  const [listClient, setListClient] = useState([]);
+
+  useEffect(() => {
+    socket.on('list-client', (data) => {
+      console.log(data);
+    });
+  }, []);
+
   return (
-    <Modal title="List Perangkat Tertaut" open={state}>
+    <Modal
+      title="List Perangkat Tertaut"
+      open={state}
+      onOk={() => setState(false)}
+      onCancel={() => setState(false)}
+    >
       <p>Contoh</p>
     </Modal>
   );
