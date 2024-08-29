ec4d81ef66f807e3e3d47cb4a1ccee3a8cedeef6
Z4nR <ari.comunity1@gmail.com>
2024-08-29 13:52:50 +0700

    update compare types

---
+++ client/src/utils/state/compare/compare.types.ts
@@ -0,0 +1,43 @@
+import { GetProp, TableColumnsType, TableProps, TransferProps } from 'antd';
+
+export type TransferItem = GetProp<TransferProps, 'dataSource'>[number];
+
+export interface DataType {
+  placeId: string;
+  placeName: string;
+  placeAddress: string;
+  placeMap: {
+    place_geojson: any;
+  };
+  type: {
+    name: string;
+    label: string;
+  };
+  rangePlace: string;
+}
+
+export interface TableTransferProps extends TransferProps<TransferItem> {
+  dataSource: DataType[];
+  leftColumns: TableColumnsType<DataType>;
+  rightColumns: TableColumnsType<DataType>;
+}
+
+export interface DataUserPlaceType {
+  placeId: string;
+  placeName: string;
+  placeAddress: string;
+  placeCenterPoint: any;
+  placeMap: {
+    place_geojson: any;
+  };
+  type: {
+    name: string;
+    label: string;
+  };
+  createdAt: Date;
+}
+
+export interface TableUserProps extends TableProps {
+  dataSource: DataUserPlaceType[];
+  columns: TableColumnsType<DataUserPlaceType>;
+}
