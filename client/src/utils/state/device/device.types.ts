c8f2149969cd2092c9ef84a64babe5935ed07e58
Z4nR <ari.comunity1@gmail.com>
2024-09-25 13:30:39 +0700

    update device type

--- client/src/utils/state/device/device.types.ts
+++ client/src/utils/state/device/device.types.ts
@@ -1,10 +1,12 @@
 export interface DeviceData {
+  uniqueCode: string;
   device: string;
   type: string;
   mobile: boolean;
 }
 
 export interface DeviceState {
+  uniqueCode: string;
   device: string;
   type: string;
   mobile: boolean;
