98d676b7ad46e50423381df3038d8850628bc99e
Z4nR <ari.comunity1@gmail.com>
2024-10-06 01:09:50 +0700

    add statistic profile page

---
+++ client/src/components/modal/StatisticProfile.tsx
@@ -0,0 +1,64 @@
+import { dateFormatter } from '@/utils/helper';
+import { userDetail } from '@/utils/networks';
+import { useQuery } from '@tanstack/react-query';
+import { Descriptions, DescriptionsProps, Modal, Skeleton } from 'antd';
+
+interface ModalSet {
+  setState: (state: boolean) => void;
+  state: boolean;
+  id: string;
+}
+
+const StatisticProfile: React.FC<ModalSet> = ({ id, state, setState }) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['user-mini-profile', id],
+    queryFn: async () => await userDetail(id),
+  });
+
+  const itemDescSimple: DescriptionsProps['items'] = [
+    {
+      key: '1',
+      label: 'Dibuat Pada',
+      children: dateFormatter(data?.createdAt),
+    },
+    {
+      key: '2',
+      label: 'Nama Akun',
+      children: data?.userName,
+    },
+    {
+      key: '3',
+      label: 'Nama Pengguna',
+      children: data?.fullName,
+    },
+    {
+      key: '4',
+      label: 'Deskripsi Akun',
+      children: data?.description ? data?.description : '-',
+    },
+    {
+      key: '5',
+      label: 'Jumlah Tempat',
+      children: data?.Count.place,
+    },
+  ];
+
+  return (
+    <Modal
+      title="Profil Singkat"
+      open={state}
+      maskClosable={false}
+      centered
+      footer={null}
+      onCancel={() => {
+        setState(false);
+      }}
+    >
+      <Skeleton loading={isLoading} paragraph={{ rows: 4 }}>
+        <Descriptions layout="vertical" size="small" items={itemDescSimple} />
+      </Skeleton>
+    </Modal>
+  );
+};
+
+export default StatisticProfile;
