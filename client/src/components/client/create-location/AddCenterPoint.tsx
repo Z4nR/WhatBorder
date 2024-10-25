d2cafb8ab66d6bd4f2443634100f81121d230892
Z4nR <ari.comunity1@gmail.com>
2024-10-25 09:03:51 +0700

    update add center

--- client/src/components/client/create-location/AddCenterPoint.tsx
+++ client/src/components/client/create-location/AddCenterPoint.tsx
@@ -30,10 +30,24 @@ const AddCenterPoint: React.FC<SocketConnect> = ({ client, desktop }) => {
       },
       (error) => {
         setLoading(false);
-        console.error(`Error getting location: ${error.message}`);
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            console.error('User denied the request for Geolocation.');
+            break;
+          case error.POSITION_UNAVAILABLE:
+            console.error('Location information is unavailable.');
+            break;
+          case error.TIMEOUT:
+            console.error('The request to get user location timed out.');
+            break;
+          default:
+            console.error('An unknown error occurred.');
+            break;
+        }
       },
       {
         enableHighAccuracy: true,
+        timeout: 10000,
         maximumAge: 0,
       }
     );
