97b170957c93f0d4faed482fd4fb7708ec72e20a
Z4nR <ari.comunity1@gmail.com>
2024-10-29 08:53:43 +0700

    update map view

--- client/src/components/desktop/create-location/MapView.tsx
+++ client/src/components/desktop/create-location/MapView.tsx
@@ -1,12 +1,27 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 
-const MapView: React.FC = () => {
+interface CreateMapView {
+  centerPoint: [number, number];
+  mapData: any;
+}
+
+const MapView: React.FC<CreateMapView> = ({ centerPoint, mapData }) => {
   return (
-    <MapContainer center={[-1.2480891, 118]} zoom={4} scrollWheelZoom={true}>
+    <MapContainer
+      center={centerPoint ? centerPoint : [-1.2480891, 118]}
+      zoom={4}
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {mapData && (
+        <GeoJSON
+          key={`map-data-${new Date().getMilliseconds()}`}
+          data={mapData}
+        />
+      )}
     </MapContainer>
   );
 };
