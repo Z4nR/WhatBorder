869bf09941687c5a720f440061efd14f7c00788c
Z4nR <ari.comunity1@gmail.com>
2024-11-01 23:54:41 +0700

    update map detail

--- client/src/components/general/map/MapDetail.tsx
+++ client/src/components/general/map/MapDetail.tsx
@@ -16,7 +16,7 @@ interface MapDetailProps {
 
 const MapDetail: React.FC<MapDetailProps> = ({ data, placeMap, position }) => {
   return (
-    <MapContainer center={[-1.2480891, 118]} zoom={17} scrollWheelZoom={true}>
+    <MapContainer center={[-1.2480891, 118]} zoom={17} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
