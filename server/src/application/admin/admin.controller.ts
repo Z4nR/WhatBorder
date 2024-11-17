4e01be47c47acb38ab70b5a8e3cf448cd27d877c
Z4nR <ari.comunity1@gmail.com>
2024-11-17 23:28:30 +0700

    update admin controller

--- server/src/application/admin/admin.controller.ts
+++ server/src/application/admin/admin.controller.ts
@@ -22,7 +22,6 @@ import { Request } from 'express';
 export class AdminController {
   constructor(
     private readonly adminService: AdminService,
-    private readonly helperService: HelperService,
     private readonly placeService: PlaceService,
   ) {}
 
@@ -43,11 +42,8 @@ export class AdminController {
     const user = req['user'];
     const userId = user.sub;
 
-    const findUser = await this.helperService.findByIdUser(userId);
-    if (!findUser) throw new NotFoundException('Pengguna Tidak Ditemukan');
-
-    const findPlace = await this.placeService.findOne(id);
-    if (!findPlace) throw new NotFoundException('Tempat Tidak Ditemukan');
+    await this.adminService.validateUserAccount(userId);
+    await this.adminService.validatePlaceData(id);
 
     return await this.adminService.removePlace(id);
   }
@@ -57,11 +53,7 @@ export class AdminController {
     const user = req['user'];
     const userId = user.sub;
 
-    const findUser = await this.helperService.findByIdUser(userId);
-    if (!findUser) throw new NotFoundException('Pengguna Tidak Ditemukan');
-    if (findUser.admin)
-      throw new BadRequestException('Tidak Dapat Menghapus Sesama Admin');
-
+    await this.adminService.validateUserAccount(userId);
     return this.adminService.removeUser(id);
   }
 }
