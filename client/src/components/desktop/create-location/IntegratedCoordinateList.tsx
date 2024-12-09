f73bb72c4759c63d074eac3b4d038e8298d037f5
Z4nR <ari.comunity1@gmail.com>
2024-12-09 08:29:40 +0700

    update page to create new place

--- client/src/components/desktop/create-location/IntegratedCoordinateList.tsx
+++ client/src/components/desktop/create-location/IntegratedCoordinateList.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
-import { Button, Flex, Form, Input, Typography } from 'antd';
-import { IntegratedCoordinateListProps } from '@/utils/types/utils.types';
+import { Form, Typography } from 'antd';
+import CoordinateField from './CoordinateField';
 
 const { Text } = Typography;
 
-const IntegratedCoordinateList: React.FC<IntegratedCoordinateListProps> = ({
-  addRef,
-  disable,
-}) => {
+const IntegratedCoordinateList: React.FC<{
+  addRef: React.MutableRefObject<(fieldsValue?: any, index?: number) => void>;
+}> = ({ addRef }) => {
   return (
     <>
       <Text>Titik Koordinat Batas Tempat (Longitude, Latitude)</Text>
-      <div
-        style={{ minHeight: '15.2vh', maxHeight: '60vh', overflowY: 'scroll' }}
-      >
+      <div style={{ maxHeight: '60vh', overflowY: 'scroll' }}>
         <Form.List
           name="longlat"
           rules={[
             {
-              validator: async (_, latlong) => {
-                if (!latlong || latlong.length < 3) {
+              validator: async (_, longlat) => {
+                if (!longlat || longlat.length < 3) {
                   return Promise.reject(
                     new Error('Setidaknya Masukkan Tiga Titik Koordinat')
                   );
@@ -35,74 +31,16 @@ const IntegratedCoordinateList: React.FC<IntegratedCoordinateListProps> = ({
             return (
               <>
                 {fields.map((field) => (
-                  <Form.Item
-                    style={{ marginBottom: 0 }}
-                    required={true}
-                    key={field.name}
-                  >
-                    <Flex
-                      gap={'middle'}
-                      style={{ width: '100%', marginTop: '0.5rem' }}
-                    >
-                      <Form.Item
-                        {...field}
-                        name={[field.name, 0]} // First part of the coordinate
-                        validateTrigger={['onChange', 'onBlur']}
-                        rules={[
-                          {
-                            required: true,
-                            message: 'Harap masukkan nilai longitude.',
-                          },
-                        ]}
-                        style={{ width: '100%', marginBottom: 0 }}
-                        key={`${field.key}-integrated-longitude`}
-                      >
-                        <Input
-                          placeholder="Longitude"
-                          style={{ width: '100%' }}
-                        />
-                      </Form.Item>
-                      <Form.Item
-                        {...field}
-                        name={[field.name, 1]} // Second part of the coordinate
-                        validateTrigger={['onChange', 'onBlur']}
-                        rules={[
-                          {
-                            required: true,
-                            message: 'Harap masukkan nilai latitude.',
-                          },
-                        ]}
-                        style={{ width: '100%', marginBottom: 0 }}
-                        key={`${field.key}-integrated-latitude`}
-                      >
-                        <Input
-                          placeholder="Latitude"
-                          style={{ width: '100%' }}
-                        />
-                      </Form.Item>
-
-                      {fields.length > 1 ? (
-                        <MinusCircleOutlined
-                          className="dynamic-delete-button"
-                          onClick={() => remove(field.name)}
-                        />
-                      ) : null}
-                    </Flex>
-                  </Form.Item>
+                  <CoordinateField
+                    key={field.key}
+                    fields={fields}
+                    field={field}
+                    remove={remove}
+                  />
                 ))}
-                {!disable && (
-                  <Form.Item>
-                    <Button
-                      type="dashed"
-                      onClick={() => add()}
-                      style={{ width: '60%', marginTop: '10px' }}
-                      icon={<PlusOutlined />}
-                    >
-                      Add field
-                    </Button>
-                    <Form.ErrorList errors={errors} />
-                  </Form.Item>
-                )}
+                <Form.Item>
+                  <Form.ErrorList errors={errors} />
+                </Form.Item>
               </>
             );
           }}
--- client/src/components/desktop/create-location/ManualCoordinateList.tsx
+++ client/src/components/desktop/create-location/ManualCoordinateList.tsx
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, Form, Typography } from 'antd';
+import { Button, Flex, Form, FormInstance, Input, Typography } from 'antd';
 import CoordinateField from './CoordinateField';
 
 const { Text } = Typography;
 
-const ManualCoordinateList: React.FC = () => {
+const ManualCoordinateList: React.FC<{ form: FormInstance }> = ({ form }) => {
+  const addRef = useRef<(fieldsValue?: any, index?: number) => void>();
+
+  const handleRef = () => {
+    const values = form.getFieldsValue(['longmanual', 'latmanual']);
+    console.log('Longitude:', Number(values.longmanual));
+    console.log('Latitude:', Number(values.latmanual));
+
+    // Make sure the `add` function expects an array of objects with the right structure
+    if (addRef.current) {
+      addRef.current([Number(values.longmanual), Number(values.latmanual)]);
+    }
+  };
+
   return (
     <>
       <Text>Titik Koordinat Batas Tempat (Longitude, Latitude)</Text>
-      <div
-        style={{ minHeight: '15.2vh', maxHeight: '60vh', overflowY: 'scroll' }}
-      >
+      <Form.Item style={{ marginBottom: 0 }} required={true}>
+        <Flex gap={'middle'} style={{ width: '100%', marginTop: '0.5rem' }}>
+          <Form.Item
+            name="longmanual"
+            validateTrigger={['onChange', 'onBlur']}
+            rules={[
+              {
+                required: true,
+                message: 'Harap masukkan nilai longitude.',
+              },
+            ]}
+            style={{ width: '100%', marginBottom: 0 }}
+          >
+            <Input placeholder="Longitude" style={{ width: '100%' }} />
+          </Form.Item>
+          <Form.Item
+            name="latmanual"
+            validateTrigger={['onChange', 'onBlur']}
+            rules={[
+              {
+                required: true,
+                message: 'Harap masukkan nilai latitude.',
+              },
+            ]}
+            style={{ width: '100%', marginBottom: 0 }}
+          >
+            <Input placeholder="Latitude" style={{ width: '100%' }} />
+          </Form.Item>
+        </Flex>
+      </Form.Item>
+      <Form.Item>
+        <Button
+          type="dashed"
+          onClick={() => handleRef()}
+          style={{ width: '60%', marginTop: '10px' }}
+          icon={<PlusOutlined />}
+        >
+          Tambah Koordinat
+        </Button>
+      </Form.Item>
+      <div style={{ maxHeight: '60vh', overflowY: 'scroll' }}>
         <Form.List
           name="longlat"
           rules={[
             {
-              validator: async (_, latlong) => {
-                if (!latlong || latlong.length < 3) {
+              validator: async (_, longlat) => {
+                if (!longlat || longlat.length < 3) {
                   return Promise.reject(
                     new Error('Setidaknya Masukkan Tiga Titik Koordinat')
                   );
@@ -27,24 +78,18 @@ const ManualCoordinateList: React.FC = () => {
           ]}
         >
           {(fields, { add, remove }, { errors }) => {
+            addRef.current = add;
             return (
               <>
                 {fields.map((field) => (
                   <CoordinateField
+                    key={field.key}
                     fields={fields}
                     field={field}
                     remove={remove}
                   />
                 ))}
                 <Form.Item>
-                  <Button
-                    type="dashed"
-                    onClick={() => add()}
-                    style={{ width: '60%', marginTop: '10px' }}
-                    icon={<PlusOutlined />}
-                  >
-                    Add field
-                  </Button>
                   <Form.ErrorList errors={errors} />
                 </Form.Item>
               </>
