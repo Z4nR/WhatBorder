2c1e769e38fc6e86e20e37164ac7fb3525a7b11c
Z4nR <ari.comunity1@gmail.com>
2025-03-12 00:15:22 +0700

    update admin dashboard

--- client/src/components/general/dashboard/AdminDashboard.tsx
+++ client/src/components/general/dashboard/AdminDashboard.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getGreeting } from '@/utils/helper';
 import useUserState from '@/utils/state/userState';
-import { Card, Col, Flex, Row, Statistic, Tooltip, Typography } from 'antd';
+import {
+  Card,
+  Col,
+  Flex,
+  Row,
+  Skeleton,
+  Statistic,
+  Tooltip,
+  Typography,
+} from 'antd';
 import { CompassOutlined, TeamOutlined, HomeOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
+import { DashboardChartProps } from '@/utils/types/statistic.types';
+import { placeStatisticAdmin } from '@/utils/networks';
+import { useQuery } from '@tanstack/react-query';
+import { Chart } from '@antv/g2';
 
 const { Title } = Typography;
 
@@ -33,6 +46,50 @@ const AdminDashboard: React.FC = () => {
     </Tooltip>,
   ];
 
+  const { data, isLoading } = useQuery({
+    queryKey: ['place-statistic-admin'],
+    queryFn: async () => await placeStatisticAdmin(),
+  });
+
+  useEffect(() => {
+    if (!data) return;
+
+    const chart = new Chart({
+      container: 'statistic',
+      autoFit: true,
+    });
+
+    const chartData = data?.detail.map((item: DashboardChartProps) => ({
+      Jenis: item.buildingName,
+      Jumlah: item.placeCount,
+      Bulan: item.month,
+      color: item.color,
+    }));
+
+    // Extract unique colors from transformedData
+    const uniqueColors = [
+      ...new Set(chartData.map((item: DashboardChartProps) => item.color)),
+    ];
+
+    chart
+      .line()
+      .data(chartData)
+      .encode('x', 'Bulan')
+      .encode('y', 'Jumlah')
+      .encode('color', 'Jenis')
+      .scale('color', {
+        range: uniqueColors,
+      })
+      .style('minHeight', 10)
+      .animate('enter', { type: 'pathIn', duration: 1000 });
+
+    chart.render();
+
+    return () => {
+      chart.destroy();
+    };
+  }, [data]);
+
   return (
     <div style={{ minHeight: '100dvh' }}>
       <Row gutter={[16, 16]} wrap>
@@ -42,10 +99,30 @@ const AdminDashboard: React.FC = () => {
               {greeting}, {username}
             </Title>
             <Flex gap={30} style={{ marginTop: '8px' }} wrap>
-              <Statistic title={'Total Tempat'} value={2} />
-              <Statistic title={'Baru Ditambahkan Bulan Ini'} value={2} />
+              <Statistic title={'Total Tempat'} value={data?.totalPlace} />
+              <Statistic
+                title={'Baru Ditambahkan Bulan Ini'}
+                value={data?.totalPlaceThisMonth}
+              />
+              <Statistic
+                title={'Persentase Perubahan Data'}
+                value={parseFloat(data?.percentageComparison)}
+                precision={2}
+                suffix="%"
+                valueStyle={{
+                  color:
+                    data?.statusPercentage === 'increase'
+                      ? 'green'
+                      : data?.statusPercentage === 'decrease'
+                      ? 'red'
+                      : 'black',
+                }}
+              />
             </Flex>
           </Card>
+          <Skeleton style={{ marginTop: '1rem' }} loading={isLoading} active>
+            <div id="statistic"></div>
+          </Skeleton>
         </Col>
       </Row>
     </div>
