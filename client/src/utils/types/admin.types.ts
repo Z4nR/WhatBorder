aea8773d6b8bec0b28a893156174916fd37825ab
Z4nR <ari.comunity1@gmail.com>
2025-03-26 20:55:00 +0700

    update admin type

--- client/src/utils/types/admin.types.ts
+++ client/src/utils/types/admin.types.ts
@@ -1,3 +1,5 @@
+import { DashboardChartProps, PlaceListProps } from './statistic.types';
+
 export interface PlaceTypeCreateProps {
   namecreate: string;
   labelcreate: string;
@@ -8,4 +10,27 @@ export interface PlaceTypeUpdateProps {
   nameupdate: string;
   labelupdate: string;
   colorupdate: string;
+}
+
+export interface DashboardAdminChartProps {
+  data: {
+    totalPlace: number;
+    totalPlaceThisMonth: number;
+    percentageComparison: string;
+    statusPercentage: string;
+    chartCreate: DashboardChartProps[];
+    chartUpdate: DashboardChartProps[];
+    chartType: DashboardChartProps[];
+    chartArea: DashboardAdminMapProps[];
+    newPlace: PlaceListProps[];
+  };
+  loading: boolean;
+}
+
+export interface DashboardAdminMapProps {
+  place_id: string;
+  place_name: string;
+  place_owner: string;
+  place_center_long: number;
+  place_center_lat: number;
 }
