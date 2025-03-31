b5048f105b9ab209c6df3c7441891a0ffbf5438f
Z4nR <ari.comunity1@gmail.com>
2025-03-31 16:11:09 +0700

    update user role list

--- client/src/components/general/admin/user-action/UserRoleList.tsx
+++ client/src/components/general/admin/user-action/UserRoleList.tsx
@@ -1,6 +1,15 @@
+import { adminUserRoleList } from '@/utils/networks';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const UserRoleList: React.FC = () => {
+  const { data } = useQuery({
+    queryKey: ['user-list', 'only'],
+    queryFn: async () => await adminUserRoleList(),
+  });
+
+  console.log(data);
+
   return <p>Ini Halaman Role User</p>;
 };
 
