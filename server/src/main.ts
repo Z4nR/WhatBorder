5fde092f243d046fe1fea149eab368e061dbc498
Z4nR <ari.comunity1@gmail.com>
2025-05-10 00:26:16 +0700

    update main file for setup versioning

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
+  const origin = process.env.ORIGIN.split(', ');
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log(origin);
+  }
+
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
-  app.setGlobalPrefix('v1');
-  await app.listen(5555);
+  app.enableCors({
+    origin: process.env.NODE_ENV === 'development' ? '*' : origin,
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
