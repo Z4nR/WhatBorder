3063957245287878059ea28932733e98b6263592
Z4nR <ari.comunity1@gmail.com>
2025-05-28 08:57:07 +0700

    delete unstructure well folder

--- server/src/application/auth/authorize/authorize.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthorizeService } from './authorize.service';
-import { AuthorizeController } from './authorize.controller';
-
-@Module({
-  controllers: [AuthorizeController],
-  providers: [AuthorizeService],
-})
-export class AuthorizeModule {}
--- server/src/application/auth/authorize/dto/update-authorize.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAuthorizeDto } from './create-authorize.dto';
-
-export class UpdateAuthorizeDto extends PartialType(CreateAuthorizeDto) {}
