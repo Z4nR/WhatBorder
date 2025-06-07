ec073ee957d49a88b7b7d4e18af663423d8efe79
Z4nR <ari.comunity1@gmail.com>
2025-06-07 22:15:28 +0700

    update userState

--- client/src/utils/state/userState.ts
+++ client/src/utils/state/userState.ts
@@ -1,15 +1,15 @@
 import { create } from 'zustand';
-import { UserProps, UserState } from '../../types/user.types';
 import { createJSONStorage, persist } from 'zustand/middleware';
+import { UserProps, UserState } from '../types/user.types';
 
 const useUserState = create<UserState>()(
   persist(
     (set) => ({
-      role: false,
+      role: 3,
       name: '',
       exp: 0,
       setUser: (state: UserProps) => set(() => ({ ...state })),
-      clearUser: () => set(() => ({ role: false, exp: 0, name: '' })),
+      clearUser: () => set(() => ({ role: 3, exp: 0, name: '' })),
     }),
     {
       name: 'user',
