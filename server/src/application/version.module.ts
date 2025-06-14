9cb3e05fa3bbb6d5d224a005f69bb20dfec893af
Z4nR <ari.comunity1@gmail.com>
2025-06-14 12:00:00 +0700

    update version module

--- server/src/application/version.module.ts
+++ server/src/application/version.module.ts
@@ -1,22 +1,19 @@
-import { AuthModule } from './auth/authentic/auth.module';
 import { PlaceModule } from './place/place.module';
 import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
 import { WebsocketModule } from './websocket/websocket.module';
-import { AuthorizeModule } from './auth/authorize/authorize.module';
 import { AuthzModule } from './authz/authz.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     AuthModule,
-    AuthorizeModule,
+    AuthzModule,
     PlaceModule,
     UserModule,
     AdminModule,
     WebsocketModule,
-    AuthzModule,
   ],
 })
 export class VersionModule {}
