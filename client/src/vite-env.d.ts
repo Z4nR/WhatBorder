1c130c91dd4c6d451a70c9287760bee5930f95aa
Z4nR <ari.comunity1@gmail.com>
2025-08-17 15:07:59 +0700

    update vite env

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_BACKEND: string;
+  readonly VITE_WSS: string;
   readonly VITE_SECRET: string;
   readonly VITE_USER: string;
 }
