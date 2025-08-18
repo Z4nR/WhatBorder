fc8de77c415adef81646e7638697ffb4a6e1be2d
Z4nR <ari.comunity1@gmail.com>
2025-08-18 18:46:07 +0700

    add spin to dashboard

--- client/src/pages/DashboardPages.tsx
+++ client/src/pages/DashboardPages.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import AdminDashboard from '@/components/general/dashboard/admin/AdminDashboard';
 import UserDashboard from '@/components/general/dashboard/user/UserDashboard';
 import useUserState from '@/utils/state/userState';
-import Loading from '@/components/general/utils/Loading';
+import { Spin } from 'antd';
 
 const DashboardPages: React.FC = () => {
   const role = useUserState().role;
 
   if (role === undefined || role === null) {
     // while role is still loading, don't render dashboards
-    return <Loading />;
+    return (
+      <div style={{ minHeight: '100dvh' }}>
+        <Spin style={{ margin: '0 auto' }} tip="Memuat..." size="large">
+          <div className="content" />
+        </Spin>
+      </div>
+    );
   }
 
   return role === 3 ? <UserDashboard /> : <AdminDashboard />;
