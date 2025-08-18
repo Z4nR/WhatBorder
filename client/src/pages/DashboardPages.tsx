9221fcb7b1407879e916e06261697cd43c045102
Z4nR <ari.comunity1@gmail.com>
2025-08-18 18:39:17 +0700

    update dashboard page

--- client/src/pages/DashboardPages.tsx
+++ client/src/pages/DashboardPages.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import AdminDashboard from '@/components/general/dashboard/admin/AdminDashboard';
 import UserDashboard from '@/components/general/dashboard/user/UserDashboard';
 import useUserState from '@/utils/state/userState';
+import Loading from '@/components/general/utils/Loading';
 
 const DashboardPages: React.FC = () => {
   const role = useUserState().role;
+
+  if (role === undefined || role === null) {
+    // while role is still loading, don't render dashboards
+    return <Loading />;
+  }
+
   return role === 3 ? <UserDashboard /> : <AdminDashboard />;
 };
 
