4c3a68e16c19bb055685ae6623095d90a016aca5
Z4nR <ari.comunity1@gmail.com>
2025-08-30 11:12:59 +0700

    update admin dto

--- server/src/application/admin/dto/create-admin.dto.ts
+++ server/src/application/admin/dto/create-admin.dto.ts
@@ -1,7 +1,5 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
-export class CreateAdminDto {}
-
 export class AddBuildingDto {
   @IsString()
   @IsNotEmpty()
--- server/src/application/admin/dto/update-admin.dto.ts
+++ server/src/application/admin/dto/update-admin.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateAdminDto } from './create-admin.dto';
+import { AddBuildingDto } from './create-admin.dto';
 
-export class UpdateAdminDto extends PartialType(CreateAdminDto) {}
+export class UpdateBuildingDto extends PartialType(AddBuildingDto) {}
