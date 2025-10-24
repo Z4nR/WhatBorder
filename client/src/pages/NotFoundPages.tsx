19f5f60d82fe329dd1394815bb31dc4996b5f479
Z4nR <ari.comunity1@gmail.com>
2025-10-24 12:00:00 +0700

    update url not found pages

--- client/src/pages/NotFoundPages.tsx
+++ client/src/pages/NotFoundPages.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Button, Result } from 'antd';
-import { useNavigate } from 'react-router-dom';
 
 const NotFoundPages: React.FC = () => {
-  const navigate = useNavigate();
-
   return (
     <Result
       style={{ marginTop: '7%' }}
@@ -12,8 +9,8 @@ const NotFoundPages: React.FC = () => {
       title="404"
       subTitle="Maaf, halaman yang anda kunjungi tidak ditemukan."
       extra={
-        <Button type="primary" onClick={() => navigate('/', { replace: true })}>
-          Kembali ke Dashboard
+        <Button type="primary" onClick={() => window.history.back()}>
+          Kembali ke Halaman Sebelumnya
         </Button>
       }
     />
