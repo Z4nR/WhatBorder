54c1b2af0af8aeae0c018436d0e9b4284c45b6e9
Z4nR <ari.comunity1@gmail.com>
2025-10-29 12:00:00 +0700

    setup menu setting pages

--- client/src/pages/super-admin/menu-setting/MenuSettingPages.tsx
+++ client/src/pages/super-admin/menu-setting/MenuSettingPages.tsx
@@ -1,6 +1,13 @@
+// import { superMenuList } from '@/utils/networks';
+// import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const MenuSettingPages: React.FC = () => {
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ['role-access-list'],
+  //   queryFn: async () => await superMenuList(),
+  // });
+
   return <div>Comingsoon</div>;
 };
 
